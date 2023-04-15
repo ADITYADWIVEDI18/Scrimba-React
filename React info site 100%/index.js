545d32f82cca964f93a2af5824f611c76e8ee8f7
Aditya Dwivedi <104710359+ADITYADWIVEDI18@users.noreply.github.com>
2023-04-15 22:06:08 +0530

    Add files via upload

---
+++ React info site 100%/index.js
@@ -0,0 +1,8 @@
+$(window).on("load",function(){
+    $(".loader-wrapper").fadeOut("slow");
+});
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("lightmode");}
+
